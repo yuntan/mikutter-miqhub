6f5ed773d01bd61a82cbe009c3773d76c62ab52b
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-02 18:47:08 +0900

    modelviewerの定義を別ファイルに分離，Ownerのmodelviewerの定義を追加
    レイアウトを修正

--- miqhub.rb
+++ miqhub.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'command'
-require_relative 'model/repository'
+require_relative 'modelviewer'
 
 module Plugin::MiqHub
   PM = Plugin::MiqHub
@@ -9,21 +9,4 @@ end
 
 Plugin.create :miqhub do
   PM = Plugin::MiqHub
-
-  defmodelviewer PM::Repository do |repo|
-    [
-      [_('名前'), repo.name],
-      [_('説明'), repo.description],
-      ['Star', repo.star_count],
-      [_('最終更新日'), repo.modified],
-      [_('作成日'), repo.created],
-    ]
-  end
-
-  deffragment PM::Repository, :readme, _('説明') do |repo|
-    set_icon repo.icon
-    nativewidget((Gtk::Label.new repo.description).tap do |label|
-      label.selectable = true
-    end)
-  end
 end
---
+++ modelviewer.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'model/owner'
+require_relative 'model/repository'
+
+Plugin.create :miqhub do
+  defmodelviewer PM::Repository do |repo|
+    [
+      [_('名前'), repo.name_with_owner],
+      [_('説明'), repo.description],
+      ['Star', repo.star_count],
+      ['Fork', repo.fork_count],
+      [_('最終更新日'), repo.modified],
+      [_('作成日'), repo.created],
+      ['URL', repo.url],
+    ]
+  end
+
+  deffragment PM::Repository, :readme, _('説明') do |repo|
+    set_icon repo.icon
+    label = Gtk::Label.new.tap do |label|
+      label.text = repo.description
+      label.wrap = true
+      label.selectable = true
+    end
+    nativewidget Gtk::VBox.new.closeup label
+  end
+
+  defmodelviewer PM::Owner do |owner|
+    [
+      [(_ '名前'), owner.name],
+      ['URL', owner.url],
+    ]
+  end
+end
