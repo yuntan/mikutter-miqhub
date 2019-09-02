8b49fdddee87243eef90afd7fe220e1278d39440
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-03 01:27:27 +0900

    APIクラスを実装
    認証したユーザーの情報を取得するfetch_me!関数を追加

--- api.rb
+++ api.rb
@@ -8,81 +8,122 @@ require_relative 'model/repository'
 require_relative 'model/owner'
 
 module Plugin::MiqHub
-  ENDPOINT = URI.parse 'https://api.github.com/graphql'
+  class API
+    ENDPOINT = URI.parse 'https://api.github.com/graphql'
 
-  HEADER = {
-    'Authorization' => 'bearer c291fd91e9df8f28d7a1909be19301ebbfcdddcb',
-    'Content-Type' => 'application/json',
-  }.freeze
+    QUERY_ME = <<~GRAPHQL
+      query {
+        viewer {
+          login
+          url
+          avatarUrl
+          repositories {
+            totalCount
+          }
+        }
+      }
+    GRAPHQL
 
-  QUERY = <<~GRAPHQL
-    query {
-      search(query: "topic:mikutter-plugin", type: REPOSITORY, first: 100) {
-        nodes {
-          ... on Repository {
-            name
-            nameWithOwner
-            owner {
-              login
-              url
-              avatarUrl
-            }
-            description
-            stargazers {
-              totalCount
-            }
-            viewerHasStarred
-            forkCount
-            createdAt
-            updatedAt
-            url
-            defaultBranchRef {
+    QUERY_REPOS = <<~GRAPHQL
+      query {
+        search(query: "topic:mikutter-plugin", type: REPOSITORY, first: 100) {
+          nodes {
+            ... on Repository {
               name
+              nameWithOwner
+              owner {
+                login
+                url
+                avatarUrl
+                repositories {
+                  totalCount
+                }
+              }
+              description
+              stargazers {
+                totalCount
+              }
+              viewerHasStarred
+              forkCount
+              createdAt
+              updatedAt
+              url
+              defaultBranchRef {
+                name
+              }
             }
           }
         }
       }
-    }
-  GRAPHQL
+    GRAPHQL
 
-module_function
+    def initialize(token)
+      @token = token
+    end
 
-  def fetch_repos
-    Thread.new do
+    def header
+      @header ||= {
+        'Authorization' => "bearer #{@token}",
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+      }.freeze
+    end
+
+    def fetch_me!
+      it = fetch! QUERY_ME
+      parse_owner it['data']['viewer']
+    end
+
+    def fetch_repos
+      Deferred.next do
+        it = +(fetch QUERY_REPOS)
+        it['data']['search']['nodes'].map do |it|
+          Repository.new(
+            name: it['name'],
+            name_with_owner: it['nameWithOwner'],
+            owner: (parse_owner it['owner']),
+            description: it['description'] || '',
+            star_count: it['stargazers']['totalCount'],
+            starred?: it['viewerHasStarred'],
+            fork_count: it['forkCount'],
+            created: it['createdAt'],
+            modified: it['updatedAt'],
+            url: it['url'],
+            default_branch_name: it['defaultBranchRef']['name'],
+          )
+        end
+      end
+    end
+
+  private
+
+    def fetch(query)
+      Thread.new { fetch! query }
+    end
+
+    def fetch!(query)
       body = nil
       (Net::HTTP.new ENDPOINT.host, ENDPOINT.port).tap do |http|
         http.use_ssl = true
       end.start do |http|
-        req = Net::HTTP::Post.new ENDPOINT.path, HEADER
-        req.body = { query: QUERY }.to_json
+        req = Net::HTTP::Post.new ENDPOINT.path, header
+        req.body = { query: query }.to_json
         http.request req do |res|
           res.code == '200' or next Deferred.fail res.message
           body = res.body
         end
       end
 
-      data = (JSON.parse body)['data']
-      data['search']['nodes'].map do |it|
-        Repository.new(
-          name: it['name'],
-          name_with_owner: it['nameWithOwner'],
-          owner: it['owner'].yield_self do |it|
-            Owner.new(
-              name: it['login'],
-              url: it['url'],
-              avatar_uri: it['avatarUrl'],
-            )
-          end,
-          description: it['description'] || '',
-          star_count: it['stargazers']['totalCount'],
-          starred?: it['viewerHasStarred'],
-          fork_count: it['forkCount'],
-          created: it['createdAt'],
-          modified: it['updatedAt'],
-          url: it['url'],
-          default_branch_name: it['defaultBranchRef']['name'],
-        )
-      end
+      JSON.parse body
+    end
+
+    def parse_owner(owner)
+      Owner.new(
+        idname: owner['login'],
+        url: owner['url'],
+        avatar_url: owner['avatarUrl'],
+        repo_count: owner['repositories']['totalCount'],
+      )
     end
   end
 end
