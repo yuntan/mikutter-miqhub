84644f183b9cf87ffb0ba33b5c02f9d8151c1ee2
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-02 17:55:39 +0900

    プラグインのインストール・アンインストールを実装

--- filesystem.rb
+++ filesystem.rb
@@ -1,54 +1,91 @@
 # frozen_string_literal: true
 
 require 'singleton'
+require 'rubygems/package'
+require 'zlib'
+
+require_relative 'model/repository'
 
 module Plugin::MiqHub
   class FileSystem
     include Singleton
 
+    PLUGIN_DIR = Pathname.new "#{Environment::CONFROOT}/plugin"
+
     class << self
       def installed?(idname)
         instance.installed? idname
       end
+
+      def install!(repo)
+        instance.install! repo
+      end
+
+      def uninstall!(repo)
+        instance.uninstall! repo
+      end
     end
 
     def initialize
-      @local_repos = []
-      Dir.glob "#{Environment.PLUGIN_PATH}/*/.miqhub" do |f|
-        @local_repos << (Repository.new JSON.parse f.read)
+      # :slug => RepositoryのHash
+      @local_repos = {}
+
+      Dir.glob PLUGIN_DIR / '*' / '.miqhub' do |s|
+        slug = (s.split File::Separator)[-2].to_sym
+        h = JSON.parse (IO.read s), symbolize_names: true
+        @local_repos[slug] = Repository.new h
       end
     end
 
     def installed?(idname)
-      @local_repos.any? { |repo| repo.idname == idname }
+      @local_repos.any? { |_, repo| repo.idname == idname }
     end
 
     def install!(repo)
       installed? repo.idname and return
 
+      slug = nil
       Thread.new do
-        path = Environment.PLUGIN_PATH / repo.name
-        Dir.mkdir path
-        extract_targz repo.download_uri.open, path
-      end.then do
-        @local_repos << repo
+        path = PLUGIN_DIR
+        tar_xf repo.archive_uri.open, path
+        path /= "#{repo.name}-#{repo.default_branch_name}"
+        yaml = (File.new path / '.mikutter.yml').read
+        slug = (YAML.safe_load yaml, [Symbol])['slug']
+        type_strict slug => Symbol
+        newpath = PLUGIN_DIR / slug.to_s
+        FileUtils.mv path, newpath
+        File.open newpath / '.miqhub', 'w' do |f|
+          f.write repo.to_json
+        end
+      end.next do
+        @local_repos[slug] = repo
       end
     end
 
-    def uninstall!(repo)
-      installed? repo.idname or return
+    def uninstall!(idname)
+      installed? idname or return
 
-      rmdir path
+      slug = @local_repos.find { |_, repo| repo.idname == idname }.first
+      Thread.new do
+        FileUtils.rm_r PLUGIN_DIR / slug.to_s
+      end.next do
+        @local_repos[slug] = nil
+      end
     end
 
-    def refresh!
-
-    end
+    def refresh!(repo) end
 
   private
 
-    def extract_targz(file)
-      Gem::Package::TarReader.new Zlib::GZipReader.open file
+    def tar_xf(file, path)
+      tar = Gem::Package::TarReader.new Zlib::GzipReader.open file
+      tar.rewind
+      tar.each do |entry|
+        entry.directory? and Dir.mkdir path / entry.full_name
+        entry.file? and IO.copy_stream entry, path / entry.full_name
+        entry.close
+      end
+      tar.close
     end
   end
 end
