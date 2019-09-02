3d1ba88bf9e864425252d780969267ea8d281fe9
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-03 01:01:15 +0900

    World Modelを追加

---
+++ model/world.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'owner'
+
+module Plugin::MiqHub
+  class World < Diva::Model
+    NAME = 'GitHub'
+    URL = URI.parse 'https://github.com'
+
+    register :miqhub_world, name: "#{NAME} World"
+
+    field.string :slug, required: true
+    field.has    :me, Owner, required: true
+    field.string :token, required: true
+
+    def icon
+      PM.icon :github
+    end
+
+    def title
+      NAME
+    end
+
+    def url
+      URL
+    end
+
+    alias uri url
+    alias perma_link url
+
+    def to_s
+      "#{NAME} World (#{account.name})"
+    end
+  end
+end
