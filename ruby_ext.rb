e5dde1031ce542edccdbb1d11b679c28e1b1501c
Osamu Koga <osak.63@gmail.com>
2019-09-29 13:29:21 +0900

    Fix Rubocop warnings

--- ruby_ext.rb
+++ ruby_ext.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # Enumerable#filter is available only since 2.6.0
-unless Enumerable.instance_methods.include?(:filter)
+unless Enumerable.instance_methods.include? :filter
+  # Add an alias added in Ruby >= 2.6
   module Enumerable
     alias filter select
   end
