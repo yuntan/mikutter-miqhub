2b5a5d0c9bb71277394900cdfceacecf80fa4e99
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-30 08:48:28 +0900

    refactor `require`s

--- command.rb
+++ command.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'skin'
-
 Plugin.create :miqhub do
   pm = Plugin::MiqHub
 
--- miqhub.rb
+++ miqhub.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
-require_relative 'model_ext'
-
-require_relative 'api'
-require_relative 'command'
-require_relative 'fetch'
-require_relative 'filesystem'
-require_relative 'filter'
-require_relative 'intent'
-require_relative 'modelviewer'
-require_relative 'world'
-
 module Plugin::MiqHub
   PM = Plugin::MiqHub
 end
 
+# models
+require_relative 'model_ext'
+
+# classes and modules
+require_relative 'api'
+require_relative 'filesystem'
+require_relative 'skin'
+
 Plugin.create :miqhub do
   defactivity :miqhub, 'MiqHub'
 end
+
+# plugin
+require_relative 'command'
+require_relative 'filter'
+require_relative 'intent'
+require_relative 'modelviewer'
+require_relative 'world'
