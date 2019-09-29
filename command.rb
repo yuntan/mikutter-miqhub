02a451149fd2b8af628140d23ed5ca90eea1e070
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-09-30 00:29:19 +0900

    reformat command.rb

--- command.rb
+++ command.rb
@@ -5,15 +5,18 @@ require_relative 'skin'
 Plugin.create :miqhub do
   pm = Plugin::MiqHub
 
-  command(
-    :miqhub_list,
-    name: (_ 'GitHubでプラグインを探す'),
-    condition: ->(_) { !(Plugin.filtering :miqhub_worlds, nil).first.empty? },
-    visible: true,
-    icon: PM::Skin[:github],
-    role: :window,
-  ) do
-    (Plugin.filtering :tabs, {}).first.keys.include? :miqhub and next
+  command :miqhub_list,
+          name: (_ 'GitHubでプラグインを探す'),
+          condition: proc {
+            world, = Plugin.filtering :miqhub_worlds, nil
+            !world.empty?
+          },
+          visible: true,
+          icon: pm::Skin[:github],
+          role: :window \
+  do
+    tabs, = Plugin.filtering :tabs, {}
+    tabs.keys.include? :miqhub and next
 
     tab :miqhub, 'GitHub tag:mikutter-plugin' do
       set_icon pm::Skin[:github]
@@ -30,60 +33,57 @@ Plugin.create :miqhub do
     end
   end
 
-  command(
-    :miqhub_about_owner,
-    name: lambda do |opt|
-      format (_ '%{title}について'),
-             title: (opt and opt.messages.first.owner.title or '…')
-    end,
-    condition: lambda do |opt|
-      (opt.messages.size == 1) && (opt.messages.first.is_a? PM::Repository)
-    end,
-    visible: true,
-    icon: ->(opt) { opt and opt.messages.first.owner.icon },
-    role: :timeline,
-  ) do |opt|
-    pp opt
+  command :miqhub_about_owner,
+          name: proc {
+            format (_ '%{title}について'),
+                   title: (opt and opt.messages.first.owner.title or '…')
+          },
+          condition: proc { |opt|
+            (opt.messages.size == 1) \
+              && (opt.messages.first.is_a? pm::Repository)
+          },
+          visible: true,
+          icon: proc { |opt| opt and opt.messages.first.owner.icon },
+          role: :timeline \
+  do |opt|
     Plugin.call :open, opt.messages.first.owner
   end
 
-  command(
-    :miqhub_install,
-    name: lambda do |opt|
-      format (_ '%{title}をインストール'),
-             title: (opt and opt.messages.first.title or '…')
-    end,
-    condition: lambda do |opt|
-      m = opt.messages.first
-      (opt.messages.size == 1) \
-        && (m.is_a? PM::Repository) \
-        && (not PM::FileSystem.installed? m.idname)
-    end,
-    visible: true,
-    # TODO: かわいいアイコンを描く
-    icon: PM::Skin[:install],
-    role: :timeline,
-  ) do |opt|
+  command :miqhub_install,
+          name: proc { |opt|
+            format (_ '%{title}をインストール'),
+                   title: (opt and opt.messages.first.title or '…')
+          },
+          condition: proc { |opt|
+            m = opt.messages.first
+            (opt.messages.size == 1) \
+              && (m.is_a? pm::Repository) \
+              && (not pm::FileSystem.installed? m.idname)
+          },
+          visible: true,
+          # TODO: かわいいアイコンを描く
+          icon: pm::Skin[:install],
+          role: :timeline \
+  do |opt|
     PM::FileSystem.install! opt.messages.first
   end
 
-  command(
-    :miqhub_uninstall,
-    name:  lambda do |opt|
-      format (_ '%{title}をアンインストール'),
-             title: (opt and opt.messages.first.title or '…')
-    end,
-    condition: lambda do |opt|
-      m = opt.messages.first
-      (opt.messages.size == 1) \
-        && (m.is_a? PM::Repository) \
-        && (PM::FileSystem.installed? m.idname)
-    end,
-    visible: true,
-    # TODO: かわいいアイコンを描く
-    icon: PM::Skin[:uninstall],
-    role: :timeline,
-  ) do |opt|
+  command :miqhub_uninstall,
+          name: proc { |opt|
+            format (_ '%{title}をアンインストール'),
+                   title: (opt and opt.messages.first.title or '…')
+          },
+          condition: proc { |opt|
+            m = opt.messages.first
+            (opt.messages.size == 1) \
+              && (m.is_a? pm::Repository) \
+              && (pm::FileSystem.installed? m.idname)
+          },
+          visible: true,
+          # TODO: かわいいアイコンを描く
+          icon: pm::Skin[:uninstall],
+          role: :timeline \
+  do |opt|
     PM::FileSystem.uninstall! opt.messages.first.idname
   end
 end
