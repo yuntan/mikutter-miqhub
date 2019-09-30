9386f22f5c91e446089bf804dbea6cf18f66d5d7
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-10-01 00:13:37 +0900

    fix syntax error on ruby 2.4

--- intent.rb
+++ intent.rb
@@ -3,7 +3,10 @@
 Plugin.create :miqhub do
   pm = Plugin::MiqHub
 
-  intent pm::Repository, label: (_ 'MiqHubでインストール') do |token|
+  # failed to parse with ruby 2.4
+  # rubocop:disable Style/MethodCallWithArgsParentheses
+  intent(pm::Repository, label: (_ 'MiqHubでインストール')) do |token|
     Plugin.call :miqhub_install, token.model
   end
+  # rubocop:enable Style/MethodCallWithArgsParentheses
 end
